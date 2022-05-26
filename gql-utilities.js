cabc2c18f37d346d96dc65a37dc69e85e28bf8df
VinnieKhanna <vinniek123@gmail.com>
2022-05-26 13:55:44 -0400

    more gql utilities

--- gql-utilities.js
+++ gql-utilities.js
@@ -2,6 +2,20 @@ import { gql, useQuery } from "@apollo/client";
 import client from "./apollo-client";
 
 const gql_functions = {
+    get_statuses: async () => {
+        const { data } = await client.query({
+            query: gql`
+                query getStatuses {
+                    statuses {
+                        items {
+                            id,
+                            name
+                        }
+                }
+            }`
+        });
+        return data.statuses.items;
+    },
     get_articles: async () => {
         const { data } = await client.query({
             query: gql`
@@ -18,6 +32,23 @@ const gql_functions = {
         });
         return data.articles.items
     },
+    get_users: async () => {
+        const { data } = await client.query({
+            query: gql`
+            query getUsers {
+                users {
+                    items {
+                    guid,
+                    fname,
+                    lname,
+                    email
+                    }
+                }
+            }`
+        });
+        return data.users.items;
+    },
+
     create_article: async (title, body) => {
         const { data } = await client.mutate({
             mutation: gql`
@@ -34,8 +65,80 @@ const gql_functions = {
             }`
         });
         return data;
-    }
+    },
+    create_user: async (guid, fname, lname, email) => {
+        const { data } = await client.mutate({
+            mutation: gql`
+            mutation CreateUser {
+            createUser(item: {
+                guid: ${guid},
+                fname: "${fname}",
+                lname: "${lname}",
+                email: "${email}"
+            }
+            ) {
+                guid
+            }
+        }`
+        });
+        return data;
+    },
 
+    update_article: async (id, title, body, status) => {
+        const { data } = await client.mutate({
+            mutation: gql`
+            mutation UpdateArticle {
+            updateArticle( id: ${id}, item: {
+                title: "${title}"
+              })
+              {
+                id
+              }
+            }`
+        });
+        return data;
+
+    },
+    update_user: async (guid, fname, lname, email) => {
+        const { data } = await client.mutate({
+            mutation: gql`
+            mutation UpdateUser {
+              updateUser(guid: ${guid}, item: {
+                fname: "${fname}"
+              }) 
+              {
+                guid
+              }
+            }`
+        });
+        return data;
+
+    },
+
+    delete_article: async (id) => {
+        const { data } = await client.mutate({
+            mutation: gql`
+            mutation DeleteArticle {
+              deleteArticle(id: ${id}) {
+                id
+              }
+            }`
+        });
+        return data;
+    },
+    delete_user: async (guid) => {
+        const { data } = await client.mutate({
+            mutation: gql`
+            mutation DeleteUser {
+              deleteUser(guid: ${guid}) {
+                guid
+              }
+            }`
+        });
+        return data;
+
+    }
+    
 }
 
 export default gql_functions;
