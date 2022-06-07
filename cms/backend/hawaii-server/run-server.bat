de3ff9573272e311f3179dd107beee94c1d69a31
VinnieKhanna <vinniek123@gmail.com>
2022-06-07 13:13:03 -0400

    adding backend setup scripts and configs

---
+++ cms/backend/hawaii-server/run-server.bat
@@ -0,0 +1,3 @@
+@echo off
+
+dotnet run --project w:\_git\hawaii\hawaii-gql\DataGateway.Service  --ConfigFileName w:\_git\hawaii\hawaii-samples\cms\backend\hawaii-server\hawaii-config.MsSql.json
---
+++ cms/backend/hawaii-server/run-server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet run --project /mnt/w/_git/hawaii/hawaii-gql/DataGateway.Service  --ConfigFileName /mnt/w/_git/hawaii/hawaii-samples/cms/backend/hawaii-server/hawaii-config.MsSql.json
