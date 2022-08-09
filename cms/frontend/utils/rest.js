a9a0c2e7bc9fc92a0a61332b78d3cff6fb47e767
Davide Mauri <damauri@microsoft.com>
2022-08-09 09:44:04 -0700

    read rest api address from enviroment

--- cms/frontend/utils/rest.js
+++ cms/frontend/utils/rest.js
@@ -1,7 +1,8 @@
 import { msalInstance, scopes } from "../auth_config"
+require('dotenv').config();
 
 // Hawaii's REST endpoint. Replace if running as hosted through AppService/SWA
-const REST_ENDPOINT = "https://localhost:5001/api";
+const REST_ENDPOINT = process.env.REST_ENDPOINT;
 
 // Helpers
 const rest_request_base = async (url, options, returnResponse = false) => {
