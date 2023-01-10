caaf76706e4dd2701f850d6b74207e932d93870d
Davide Mauri <damauri@microsoft.com>
2023-01-09 18:13:14 -0800

    updated to use latest dab version (0.4.11)

---
+++ todo/backend/db/azure-sql/000-database.sql
@@ -0,0 +1,27 @@
+/*
+	If running on SQL Server:
+*/
+create database DAB_Todo
+go
+
+create login dab_test_user with password = 'DAB_Pas$w0rD!'
+go
+
+use DAB_Todo
+go
+
+create user dab_test_user from login dab_test_user
+go
+
+alter role db_owner add member dab_test_user
+go
+
+/*
+	If running on Azure SQL
+*/
+
+create user dab_test_user with password = 'DAB_Pas$w0rD!'
+go
+
+alter role db_owner add member dab_test_user
+go
